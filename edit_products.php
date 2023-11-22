9ceb422210354a6998e1b601498c051afbfa2848
Mahesh <103363378+MaheshGowdaB@users.noreply.github.com>
2023-11-22 17:27:09 +0530

    commit

--- edit_products.php
+++ edit_products.php
@@ -2,22 +2,33 @@
 include "connection.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $product_code = $_POST['product_code'];
-    $product_name = $_POST['product_name'];
+    // Retrieve data from the form
+    $productCode = $_POST['product_code'];
+    $productName = $_POST['product_name'];
     $UOM = $_POST['UOM'];
     $stock = $_POST['stock'];
-    $selling_price = $_POST['selling_price'];
-    $offer_price = $_POST['offer_price'];
-    $product_image = $_POST['product_image'];
+    $sellingPrice = $_POST['selling_price'];
+    $offerPrice = $_POST['offer_price'];
+    $productImage = $_POST['product_image'];
 
-    $sql = "UPDATE tblproducts SET product_code='$product_code', product_name='$product_name', UOM='$UOM', stock='$stock', selling_price='$selling_price' , offer_price='$offer_price',  product_image='$product_image' WHERE product_code='$product_code'";
+    // Update the product details in the database
+    $sql = "UPDATE tblproducts SET 
+            product_name = '$productName',
+            UOM = '$UOM',
+            stock = '$stock',
+            selling_price = '$sellingPrice',
+            offer_price = '$offerPrice',
+            product_image = '$productImage'
+            WHERE product_code = '$productCode'";
 
     if ($conn->query($sql) === TRUE) {
-        header('Location: product_master.html');
+        echo "Product details updated successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error updating product details: " . $conn->error;
     }
-
-    $conn->close();
+} else {
+    echo "Invalid request method";
 }
+
+$conn->close();
 ?>
