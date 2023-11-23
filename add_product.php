7589a4073682aedaf4c5fb0b7476da51ce029490
Mahesh <103363378+MaheshGowdaB@users.noreply.github.com>
2023-11-23 17:20:32 +0530

    commit

--- add_product.php
+++ add_product.php
@@ -1,7 +1,6 @@
 <?php
 include "connection.php";
 
-
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
@@ -19,15 +18,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the current date and time
     $savedDateTime = date("Y-m-d H:i:s");
 
-    // Insert data into the database
-    $sql = "INSERT INTO tblproducts (product_code, product_name, UOM, stock, selling_price, offer_price, product_image, saved_by, SavedDateTime)
-            VALUES ('$product_code', '$product_name', '$UOM', '$stock', '$selling_price', '$offer_price', '$product_image', '$saved_by', '$savedDateTime')";
+    // Check if the product code already exists in the database
+    $checkDuplicateQuery = "SELECT * FROM tblproducts WHERE product_code = '$product_code'";
+    $duplicateResult = $conn->query($checkDuplicateQuery);
 
-if ($conn->query($sql) === TRUE) {
-    header('Location: product_master.html');
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    if ($duplicateResult->num_rows > 0) {
+        // Product code already exists, show an error message in a popup
+        echo "<script>
+                alert('Product code \"$product_code\" already exists. Please choose a different product code.');
+                window.history.back(); // Go back to the previous page
+              </script>";
+        exit; // Stop further execution
+    } else {
+        // Insert data into the database
+        $insertQuery = "INSERT INTO tblproducts (product_code, product_name, UOM, stock, selling_price, offer_price, product_image, saved_by, SavedDateTime)
+                        VALUES ('$product_code', '$product_name', '$UOM', '$stock', '$selling_price', '$offer_price', '$product_image', '$saved_by', '$savedDateTime')";
+
+        if ($conn->query($insertQuery) === TRUE) {
+            // Successfully added the product, redirect to the product_master.html page
+            header('Location: product_master.html');
+        } else {
+            // Error in the SQL query
+            echo "Error: " . $insertQuery . "<br>" . $conn->error;
+        }
+    }
 }
 
 // Close the database connection
