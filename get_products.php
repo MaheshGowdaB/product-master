0975df49f1803d9d419bed035cb0447347b8c921
Mahesh <103363378+MaheshGowdaB@users.noreply.github.com>
2023-11-30 11:07:01 +0530

    commit

--- get_products.php
+++ get_products.php
@@ -3,7 +3,7 @@ include "connection.php";
 
 if (isset($_GET['search']) && !empty($_GET['search'])) {
     $search_text = $_GET['search'];
-    $sql = "SELECT * FROM tblproducts WHERE 
+    $sql = "SELECT *, offer_price AS actual_offer_price FROM tblproducts WHERE 
             product_code LIKE '%$search_text%' OR
             product_name LIKE '%$search_text%' OR
             UOM LIKE '%$search_text%' OR
@@ -12,7 +12,7 @@ if (isset($_GET['search']) && !empty($_GET['search'])) {
             offer_price LIKE '%$search_text%' OR
             product_image LIKE '%$search_text%'";
 } else {
-    $sql = "SELECT * FROM tblproducts";
+    $sql = "SELECT *, offer_price AS actual_offer_price FROM tblproducts";
 }
 
 $result = $conn->query($sql);
