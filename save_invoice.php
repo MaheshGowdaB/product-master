38bf7f731415fe86df6439d2330a12313910d480
Mahesh <103363378+MaheshGowdaB@users.noreply.github.com>
2023-11-30 12:00:59 +0530

    commit

--- save_invoice.php
+++ save_invoice.php
@@ -8,62 +8,40 @@ if (isset($_POST['invoiceData']) && !empty($_POST['invoiceData'])) {
     // Initialize total quantity
     $totalQty = 0;
 
-    try {
-        // Start a database transaction
-        $conn->begin_transaction();
-
-        // Iterate through each row's data and calculate total quantity
-        foreach ($invoiceData as $rowData) {
-            $totalQty += $rowData['qty'];
-        }
-
-        // Use prepared statement for inserting data into tblinvhead
-        $insertHeaderStmt = $conn->prepare("INSERT INTO tblinvhead (InvoiceNo, InvoiceDate, CustomerName, MobileNo, Qty, NetTotal, Saved_By, SavedDateTime) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        $insertHeaderStmt->bind_param("ssssdsss", $invoiceNo, $invoiceDate, $customerName, $mobileNo, $totalQty, $netTotal, $savedBy, $savedDateTime);
-
-        // Update stock in tblproducts for each product code
-        foreach ($invoiceData as $rowData) {
-            $productCode = $rowData['productCode'];
-            $qty = $rowData['qty'];
-
-            // Use prepared statement for updating stock
-            $updateStockStmt = $conn->prepare("UPDATE tblproducts SET stock = stock - ? WHERE product_code = ?");
-            $updateStockStmt->bind_param("ss", $qty, $productCode);
-
-            // Execute the update stock query
-            if (!$updateStockStmt->execute()) {
-                throw new Exception("Error updating stock: " . $conn->error);
-            }
-
-            // Close the prepared statement
-            $updateStockStmt->close();
-        }
-
-        // Bind parameters and execute the insert header query
-        foreach ($invoiceData as $rowData) {
-            extract($rowData);
-            if (!$insertHeaderStmt->execute()) {
-                throw new Exception("Error inserting header data: " . $conn->error);
-            }
-        }
-
-        // Commit the transaction
-        $conn->commit();
-
-        // Close the prepared statement
-        $insertHeaderStmt->close();
-
-        echo 'success'; // Send a success response to the client
-    } catch (Exception $e) {
-        // Rollback the transaction on error
-        $conn->rollback();
-
-        // Output the error message
-        echo 'error: ' . $e->getMessage();
-    } finally {
-        // Close the database connection
-        $conn->close();
+    // Iterate through each row's data and calculate total quantity
+    foreach ($invoiceData as $rowData) {
+        $totalQty += $rowData['qty'];
     }
+
+    // Use the first row's data to insert a single row into tblinvhead
+    $firstRowData = $invoiceData[0];
+
+    $sql = "INSERT INTO tblinvhead (InvoiceNo, InvoiceDate, CustomerName, MobileNo, Qty, NetTotal, Saved_By, SavedDateTime)
+            VALUES ('$firstRowData[invoiceNo]', '$firstRowData[invoiceDate]', '$firstRowData[customerName]', '$firstRowData[mobileNo]', '$totalQty', '$firstRowData[netTotal]', '$firstRowData[savedBy]', '$firstRowData[savedDateTime]')";
+
+    $result = $conn->query($sql);
+
+    if (!$result) {
+        die("Invalid query: " . $conn->error);
+    }
+
+    // Update stock in tblproducts for each product code
+    foreach ($invoiceData as $rowData) {
+        $productCode = $rowData['productCode'];
+        $qty = $rowData['qty'];
+
+        $updateStockSql = "UPDATE tblproducts SET stock = stock - '$qty' WHERE product_code = '$productCode'";
+        $updateStockResult = $conn->query($updateStockSql);
+
+        if (!$updateStockResult) {
+            die("Invalid query: " . $conn->error);
+        }
+    }
+
+    // Close the database connection
+    $conn->close();
+
+    echo 'success'; // Send a success response to the client
 } else {
     // Handle the case where 'invoiceData' is not provided in the POST request
     echo 'error';
